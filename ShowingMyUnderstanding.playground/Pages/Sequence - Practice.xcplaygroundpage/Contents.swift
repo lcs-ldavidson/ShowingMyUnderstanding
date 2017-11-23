b002afb6837a9d8a296d80a7a337299cc5ac3f4d
Liam Davidson <ldavidson@lcs.on.ca>
2017-11-23 11:05:41 -0500

    All done. Easy.

--- ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
@@ -38,6 +38,65 @@ canvas.drawShapesWithBorders = false
 canvas.defaultLineWidth = 5
 
 // Add your code below... remember to use comments to indicate your intent
+
+//background
+canvas.fillColor = Color.black
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 300, height: 300)
+
+
+canvas.lineColor = Color.init(hue: 60, saturation: 80, brightness: 90, alpha: 100)
+canvas.drawLine(fromX: 127, fromY: 175, toX: 127, toY: 20)
+
+canvas.lineColor = Color.init(hue: 0, saturation: 80, brightness: 90, alpha: 100)
+canvas.drawLine(fromX: 172, fromY: 175, toX: 172, toY: 20)
+
+canvas.lineColor = Color.init(hue: 220, saturation: 90, brightness: 90, alpha: 100)
+canvas.drawLine(fromX: 217, fromY: 175, toX: 217, toY: 20)
+
+canvas.lineColor = Color.init(hue: 120, saturation: 80, brightness: 90, alpha: 100)
+canvas.drawLine(fromX: 82, fromY: 175, toX: 82, toY: 20)
+
+
+
+
+
+//YELLLOW
+canvas.fillColor = Color.init(hue: 60, saturation: 80, brightness: 90, alpha: 100)
+canvas.drawEllipse(centreX: 127, centreY: 175, width: 90, height: 90)
+
+//red
+canvas.fillColor = Color.init(hue: 0, saturation: 80, brightness: 90, alpha: 100)
+canvas.drawEllipse(centreX: 172, centreY: 175, width: 90, height: 90)
+
+//blue
+canvas.fillColor = Color.init(hue: 220, saturation: 90, brightness: 90, alpha: 100)
+canvas.drawEllipse(centreX: 217, centreY: 175, width: 90, height: 90)
+
+
+//first green lollipop
+
+canvas.fillColor = Color.init(hue: 120, saturation: 80, brightness: 90, alpha: 100)
+canvas.drawEllipse(centreX: 82, centreY: 175, width: 90, height: 90)
+
+
+
+
+//YELLLOW
+canvas.fillColor = Color.init(hue: 60, saturation: 80, brightness: 90, alpha: 70)
+canvas.drawEllipse(centreX: 127, centreY: 175, width: 90, height: 90)
+
+//red
+canvas.fillColor = Color.init(hue: 0, saturation: 80, brightness: 90, alpha: 50)
+canvas.drawEllipse(centreX: 172, centreY: 175, width: 90, height: 90)
+
+
+
+
+
+
+
+
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
