8217536c62f026c48841c8860959d0338ad707db
Liam Davidson <ldavidson@lcs.on.ca>
2017-11-23 11:17:35 -0500

    I made a face!

--- ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -24,6 +24,41 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 300)
 
 // Replace this comment and add your code below...
+//face shape
+canvas.borderColor = Color.black
+canvas.fillColor = Color.red
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 300, height: 300)
+
+//eyes
+canvas.drawShapesWithBorders = false
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 100, centreY: 200, width: 120, height: 120)
+canvas.drawEllipse(centreX: 200, centreY: 200, width: 120, height: 120)
+
+
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 100, centreY: 200, width: 30, height: 30)
+canvas.drawEllipse(centreX: 200, centreY: 200, width: 30, height: 30)
+
+
+//nose
+canvas.drawShapesWithBorders = true
+canvas.fillColor = Color.red
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 50, height: 100)
+
+//mouth
+canvas.fillColor = Color.white
+canvas.drawShapesWithBorders = false
+canvas.drawEllipse(centreX: 150, centreY: 90, width: 250, height: 100)
+canvas.fillColor = Color.red
+canvas.drawEllipse(centreX: 150, centreY: 120, width: 250, height: 100)
+
+
+
+
+
+
+
 
 /*:
  ## Template code
